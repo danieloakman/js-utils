d3d062f1861b3b3bd5ff96a7824d27ae39506eb0
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-15 13:58:12 +1000

    compile to cjs now works

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -1,8 +1,18 @@
 #! bun
 
 import { join } from 'path';
-import { parseArgs } from '../src';
+import { iife, ok, parseArgs, sh } from '../src';
 import { readdirDeep } from 'more-node-fs';
+
+const esmToCjs = iife(
+  ({ transformSync } = require('@babel/core')) =>
+    (code: string): string =>
+      ok(
+        transformSync(code, {
+          plugins: ['@babel/plugin-transform-modules-commonjs'],
+        }),
+      )?.code,
+);
 
 if (Bun.main === import.meta.path) {
   const args: { target: 'node' | 'browser' } = parseArgs(
@@ -13,22 +23,40 @@ if (Bun.main === import.meta.path) {
         choices: ['node', 'browser'],
       },
     ],
+    [
+      '--format',
+      '-f',
+      {
+        choices: ['cjs', 'esm'],
+        default: 'esm',
+      },
+    ],
   );
 
-  const srcFiles = (await readdirDeep(join(import.meta.dir, '../src'))).files;
+  const [srcFiles] = await Promise.all([
+    (await readdirDeep(join(import.meta.dir, '../src'))).files,
+    await sh('bun clean').then(ok),
+  ]);
 
-  // Bun.env.COMPILE_TARGET = args.target;
+  // Build types only:
+  ok(await sh('tsc -p tsconfig.types.json'));
 
-  const output = await Bun.build({
+  // Build
+  const { outputs } = await Bun.build({
     minify: true,
     entrypoints: srcFiles,
     outdir: '.',
     splitting: true,
+    // TODO: once bun supports cjs as a format, then put args.format here. For now babel is being used to transpile to cjs.
+    format: 'esm',
     target: args.target,
     define: {
       'Bun.env.COMPILE_TARGET': `'${args.target}'`,
     },
-    sourcemap: 'inline',
   });
-  console.log(output);
+
+  for (const output of outputs) {
+    const transpiled = esmToCjs(await output.text()).replaceAll('import.meta.require', 'require');
+    await Bun.write(output.path, transpiled);
+  }
 }
