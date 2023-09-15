96d739236ac3db3c3fcb100cebcd0b230d8f714b
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-15 16:47:10 +1000

    fixes for `main`

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -1,7 +1,13 @@
 /** Brute force way to make sure all compiled ts files are removed. `tsc --build --clean` didn't always work as intended. */
 
-import { walkdirSync } from 'more-node-fs';
-import { unlinkSync } from 'fs';
+import { walkdir } from 'more-node-fs';
+import { unlinkSync as unlink } from 'fs';
 import { join } from 'path';
-for (const { path, stats } of walkdirSync(join(import.meta.dir, '../'), { ignore: /node_modules/i }))
-  if (stats.isFile() && /(\.d\.ts|\.js|\.map)$/.test(path)) unlinkSync(path);
+
+export async function clean() {
+  for await (const { path, stats } of walkdir(join(import.meta.dir, '../'), { ignore: /node_modules/i }))
+    if (stats.isFile() && /(\.d\.ts|\.js|\.map)$/.test(path)) unlink(path);
+}
+
+// main(import.meta.path, async () => clean())
+if (import.meta.path === Bun.main) clean();
