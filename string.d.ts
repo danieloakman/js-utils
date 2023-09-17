286c193301c27bf6f95ba4e8617f82206f77d70e
danieloakman <danieloakman@users.noreply.github.com>
2023-09-17 21:24:55 +0000

    Apply automatic changes

--- string.d.ts
+++ string.d.ts
@@ -4,3 +4,11 @@
  * and non-security related tasks.
  */
 export declare function fastHash(str: string, seed?: number): number;
+/**
+ * Calls regex.exec(string) continually until there are no more matches. Differs from
+ * string.match(regex) as that only returns a string array.
+ * @param regex The regular expression to use on string. Must have the global flag set.
+ * @param string The string to search through.
+ * @throws Throws an Error if regex does not have the global flag set.
+ */
+export declare function matches(regex: RegExp, string: string): IterableIterator<RegExpExecArray>;
