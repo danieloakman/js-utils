cf04e3775b8741d8f919f5b52c5075f131486f78
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-18 08:40:06 +1000

    Fixed compiled files

---
+++ assert.js
@@ -0,0 +1,2 @@
+import{l as j} from"./functional.js";import{D as k,E as q} from"./misc.js";import"./chunk-79ee00b627f99c01.js";import{throws as G} from"assert";function B(b,h){if(!b)if(typeof h==="string")throw new Error(h);else throw h??new Error("Assertion failed")}var C=(b)=>b,D=k((b,h,r)=>j((w=q("assert"))=>w.deepStrictEqual(b,h,r)));export{G as throws,C as expectType,D as equal,B as assert};
+export{B as b,C as c,D as d,G as e};
---
+++ number.js
@@ -0,0 +1,2 @@
+import"./chunk-79ee00b627f99c01.js";function k(t){return t*h}function l(t){return t*j}function q(t,c){return t.reduce((o,p,f)=>o+Math.abs(p-c[f]),0)}var h=Math.PI/180,j=180/Math.PI;export{k as toRadians,l as toDegrees,q as manhattanDistance,h as RADIANS_MULT,j as DEGREES_MULT};
+export{h as u,j as v,k as w,l as x,q as y};
