e43db7d7dc0629c3a5146a3257e4faa3815e44db
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-18 07:33:45 +1000

    Added number.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './args';
 export * from './assert';
 export * from './functional';
 export * from './misc';
+export * from './number';
 export * from './string';
 export * from './types';
