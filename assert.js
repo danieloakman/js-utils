7ebcf4fef71cf16855b268a2e5a13204c3e88b2e
danieloakman <danieloakman@users.noreply.github.com>
2023-09-18 02:14:28 +0000

    Apply automatic changes

--- assert.js
+++ assert.js
@@ -1,3 +1,3 @@
 // @bun
-import{q as j} from"./functional.js";import{E as k,F as q} from"./misc.js";import"./chunk-6aa1f52e678a49d2.js";import{throws as G} from"assert";function B(b,h){if(!b)if(typeof h==="string")throw new Error(h);else throw h??new Error("Assertion failed")}var C=(b)=>b,D=k((b,h,r)=>j((w=q("assert"))=>w.deepStrictEqual(b,h,r)));export{G as throws,C as expectType,D as equal,B as assert};
-export{B as A,C as B,D as C,G as D};
+import{q as j} from"./functional.js";import{G as k,H as q} from"./misc.js";import"./chunk-eae73b33821ca2a4.js";import{throws as G} from"assert";function B(b,h){if(!b)if(typeof h==="string")throw new Error(h);else throw h??new Error("Assertion failed")}var C=(b)=>b,D=k((b,h,r)=>j((w=q("assert"))=>w.deepStrictEqual(b,h,r)));export{G as throws,C as expectType,D as equal,B as assert};
+export{B as C,C as D,D as E,G as F};
--- number.js
+++ number.js
@@ -1,3 +1,3 @@
 // @bun
-import"./chunk-6aa1f52e678a49d2.js";function k(t){return t*h}function l(t){return t*j}function q(t,c){return t.reduce((o,p,f)=>o+Math.abs(p-c[f]),0)}var h=Math.PI/180,j=180/Math.PI;export{k as toRadians,l as toDegrees,q as manhattanDistance,h as RADIANS_MULT,j as DEGREES_MULT};
+import"./chunk-eae73b33821ca2a4.js";function k(t){return t*h}function l(t){return t*j}function q(t,c){return t.reduce((o,p,f)=>o+Math.abs(p-c[f]),0)}var h=Math.PI/180,j=180/Math.PI;export{k as toRadians,l as toDegrees,q as manhattanDistance,h as RADIANS_MULT,j as DEGREES_MULT};
 export{h as f,j as g,k as h,l as i,q as j};
