7566914e1afc085c5caa0f91f8d5f98d848c0cf9
danieloakman <danieloakman@users.noreply.github.com>
2023-09-18 03:08:15 +0000

    Apply automatic changes

--- InMemoryCache.js
+++ InMemoryCache.js
@@ -1,2 +1,3 @@
 // @bun
-import"./functional.js";import{K as h} from"./object.js";import"./chunk-a893a07846a50da2.js";import"./chunk-2f243cc26e06d087.js";class i{map=new Map;async get(n){const p=this.map.get(n);return h(p??"")}async delete(n){return this.map.delete(n)}async set(n,p){return this.map.set(n,typeof p!=="string"?JSON.stringify(p):p),!0}async has(n){return this.map.has(n)}async clear(){return this.map.clear(),!0}async*keys(){yield*this.map.keys()}}export{i as InMemoryCache};
+import"./functional.js";import{L as h} from"./object.js";import"./chunk-cf5dc1543f4dddd4.js";import"./chunk-5cded60fc90fa3b9.js";class i{map=new Map;async get(n){const p=this.map.get(n);return h(p??"")}async delete(n){return this.map.delete(n)}async set(n,p){return this.map.set(n,typeof p!=="string"?JSON.stringify(p):p),!0}async has(n){return this.map.has(n)}async clear(){return this.map.clear(),!0}async*keys(){yield*this.map.keys()}}export{i as InMemoryCache};
+export{i as f};
--- assert.js
+++ assert.js
@@ -1,3 +1,3 @@
 // @bun
-import{l as j} from"./functional.js";import{A as q,z as k} from"./misc.js";import"./chunk-2f243cc26e06d087.js";import{throws as G} from"assert";function B(b,h){if(!b)if(typeof h==="string")throw new Error(h);else throw h??new Error("Assertion failed")}var C=(b)=>b,D=k((b,h,r)=>j((w=q("assert"))=>w.deepStrictEqual(b,h,r)));export{G as throws,C as expectType,D as equal,B as assert};
-export{B as v,C as w,D as x,G as y};
+import{m as j} from"./functional.js";import{A as k,B as q} from"./misc.js";import"./chunk-5cded60fc90fa3b9.js";import{throws as G} from"assert";function B(b,h){if(!b)if(typeof h==="string")throw new Error(h);else throw h??new Error("Assertion failed")}var C=(b)=>b,D=k((b,h,r)=>j((w=q("assert"))=>w.deepStrictEqual(b,h,r)));export{G as throws,C as expectType,D as equal,B as assert};
+export{B as w,C as x,D as y,G as z};
