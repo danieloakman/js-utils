918625d98ab443941f6b00b5211ffffbf5c782ad
danieloakman <danieloakman@users.noreply.github.com>
2023-09-18 02:14:36 +0000

    Apply automatic changes

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,6 @@ export * from './assert';
 export * from './functional';
 export * from './misc';
 export * from './number';
+export * from './object';
 export * from './string';
 export * from './types';
