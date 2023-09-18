755db34c92919a2a836c64583b9cb565ba8ab76c
danieloakman <danieloakman@users.noreply.github.com>
2023-09-18 05:05:12 +0000

    Apply automatic changes

---
+++ InMemoryCache.js
@@ -0,0 +1,2 @@
+import"./functional.js";import{L as h} from"./object.js";import"./chunk-04c73815e846c9c9.js";import"./chunk-bbbe4879b6de3a95.js";class i{map=new Map;async get(n){const p=this.map.get(n);return h(p??"")}async delete(n){return this.map.delete(n)}async set(n,p){return this.map.set(n,typeof p!=="string"?JSON.stringify(p):p),!0}async has(n){return this.map.has(n)}async clear(){return this.map.clear(),!0}async*keys(){yield*this.map.keys()}}export{i as InMemoryCache};
+export{i as f};
