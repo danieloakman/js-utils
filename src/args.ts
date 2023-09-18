dbf680892fbcf261f9c85ec8be4ae49d9a9f1724
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-18 15:14:14 +1000

    Use import instead

--- src/args.ts
+++ src/args.ts
@@ -6,12 +6,11 @@ export type AddArgumentParams =
   | [arg1: string, arg2: string, options?: ArgumentOptions];
 
 export const parseArgs = nodeOnly(
-  <T = unknown>(
+  async <T = unknown>(
     constructorParams: ConstructorParameters<typeof ArgumentParser>[0],
     ...args: AddArgumentParams[]
-  ): T => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const parser = new (require('argparse').ArgumentParser)(constructorParams);
+  ): Promise<T> => {
+    const parser = new (await import('argparse')).ArgumentParser(constructorParams);
     // @ts-ignore
     for (const arg of args) parser.add_argument(...arg);
     return parser.parse_args();
