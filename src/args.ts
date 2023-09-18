a9ee9d9bf3fd91a2258c03a032aaa0addd5ad91b
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-18 15:15:58 +1000

    Revert "Use import instead"
    This reverts commit dbf680892fbcf261f9c85ec8be4ae49d9a9f1724.

--- src/args.ts
+++ src/args.ts
@@ -6,11 +6,12 @@ export type AddArgumentParams =
   | [arg1: string, arg2: string, options?: ArgumentOptions];
 
 export const parseArgs = nodeOnly(
-  async <T = unknown>(
+  <T = unknown>(
     constructorParams: ConstructorParameters<typeof ArgumentParser>[0],
     ...args: AddArgumentParams[]
-  ): Promise<T> => {
-    const parser = new (await import('argparse')).ArgumentParser(constructorParams);
+  ): T => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const parser = new (require('argparse').ArgumentParser)(constructorParams);
     // @ts-ignore
     for (const arg of args) parser.add_argument(...arg);
     return parser.parse_args();
