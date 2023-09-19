b3ed6bd564a6f8a0220f72a349746aa07596a642
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-19 11:31:08 +1000

    compile

---
+++ assertions.js
@@ -0,0 +1,1 @@
+import{d as o} from"./functional.js";function b(d,c){if(!d)if(typeof c==="string")throw new Error(c);else throw c??new Error("Assertion failed")}var h=()=>o("Can' use `throws` "),j=(d)=>d,k=()=>o("Can't use `equal`, not implemented in browser.");export{h as throws,j as expectType,k as equal,b as assert};
--- object.js
+++ object.js
@@ -1,2 +1,2 @@
-import{d as b} from"./functional.js";import"./chunk-f1e62a083f56e81c.js";function h(K,...c){const o=c.map((i)=>[i,{}]);for(let i of K)for(let[g,N]of o){const T=typeof g==="string"?i?.[g]:g(i);N[T]=(N[T]??[]).concat(i)}return o.length<2?o[0][1]:o.map(([i,g])=>g)}function j(...K){return b(JSON.parse,...K)}export{j as safeJSONParse,h as groupBy};
+import{b} from"./functional.js";function h(K,...c){const o=c.map((i)=>[i,{}]);for(let i of K)for(let[g,N]of o){const T=typeof g==="string"?i?.[g]:g(i);N[T]=(N[T]??[]).concat(i)}return o.length<2?o[0][1]:o.map(([i,g])=>g)}function j(...K){return b(JSON.parse,...K)}export{j as safeJSONParse,h as groupBy};
 export{j as a};
