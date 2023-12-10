f1b8c2cca9c852ccc2e72e4051dafa35944d3de8
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-12-11 06:33:36 +1100

    chore: compiled for bun esm

--- InMemoryCache.js
+++ InMemoryCache.js
@@ -1,3 +1,3 @@
 // @bun
-import"./functional.js";import{T as h} from"./object.js";import"./chunk-34086439a7fedc98.js";class i{map=new Map;async get(n){const p=this.map.get(n);return h(p??"")}async delete(n){return this.map.delete(n)}async set(n,p){return this.map.set(n,typeof p!=="string"?JSON.stringify(p):p),!0}async has(n){return this.map.has(n)}async clear(){return this.map.clear(),!0}async*keys(){yield*this.map.keys()}}export{i as InMemoryCache};
-export{i as W};
+import"./functional.js";import{T as h} from"./object.js";import"./chunk-6ec6828dab5edd34.js";class i{map=new Map;async get(n){const p=this.map.get(n);return h(p??"")}async delete(n){return this.map.delete(n)}async set(n,p){return this.map.set(n,typeof p!=="string"?JSON.stringify(p):p),!0}async has(n){return this.map.has(n)}async clear(){return this.map.clear(),!0}async*keys(){yield*this.map.keys()}}export{i as InMemoryCache};
+export{i as Y};
--- assertions.js
+++ assertions.js
@@ -1,3 +1,3 @@
 // @bun
-import"./functional.js";import{Y as h} from"./chunk-34086439a7fedc98.js";function r(d,i){if(!d)if(typeof i==="string")throw new Error(i);else throw i??new Error("Assertion failed")}var w=(d,i)=>{return import.meta.require("assert").throws(d,i)},y=(d)=>d,z=(d,i,j)=>{return import.meta.require("assert").deepStrictEqual(d,i,j)};export{w as throws,y as expectType,z as equal,r as assert};
+import"./functional.js";import{_ as h} from"./chunk-6ec6828dab5edd34.js";function r(d,i){if(!d)if(typeof i==="string")throw new Error(i);else throw i??new Error("Assertion failed")}var w=(d,i)=>{return import.meta.require("assert").throws(d,i)},y=(d)=>d,z=(d,i,j)=>{return import.meta.require("assert").deepStrictEqual(d,i,j)};export{w as throws,y as expectType,z as equal,r as assert};
 export{r as a,w as b,y as c,z as d};
