a38b2f7398080f834a86d05add4b5da6c2eaf4fa
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-12-29 09:25:31 +1100

    Remove iteragain from external packages

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -46,7 +46,7 @@ export async function compile(args: CompileArgs): Promise<Error | boolean> {
     treeShaking: true,
     splitting: args.format === 'esm',
     bundle: true,
-    external: ['argparse', 'iteragain-es'],
+    external: ['argparse'],
     define: {
       'Bun.env.RUNTIME': `'${args.target}'`,
     },
