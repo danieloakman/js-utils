13728c83aac6f3a266318c9b9df9ded1fc867971
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-03 06:35:27 +1000

    Put an expect only not in CI

--- src/misc.test.ts
+++ src/misc.test.ts
@@ -24,9 +24,11 @@ describe('misc', () => {
   it('execSync', () => {
     expect(pipe(execSync('echo "hello_world"'), ok)).toInclude('hello_world');
     expect(pipe(execSync('bun -v'), ok)).toMatch(/\d+\.\d+\.\d+/);
-    expect(pipe(execSync('echo $SHELL'), ok)).toInclude(process.env.SHELL!);
     expect(execSync('a_command_that_does_not_exist')).toBeInstanceOf(Error);
     process.env.SOME_VAR = 'some_value';
     expect(pipe(execSync('echo $SOME_VAR'), ok)).toInclude('some_value');
+    if (!process.env.CI) {
+      expect(pipe(execSync('echo $SHELL'), ok)).toInclude(process.env.SHELL!);
+    }
   });
 });
