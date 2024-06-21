af50afb9605aef2f79bdafb5a61fddb9fa7ff65c
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-21 11:59:24 +1000

    build.ts now builds a bun target again

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,9 +3,10 @@ import { iife, ok } from '../src';
 import { $ } from 'bun';
 import { build as esbuild } from 'esbuild';
 import { dtsPlugin } from 'esbuild-plugin-d.ts';
+import { copyFile } from 'fs/promises';
+import { globIterateSync } from 'glob';
 import { iter } from 'iteragain';
-import { readdirDeep } from 'more-node-fs';
-import { join } from 'path';
+import { join, relative } from 'path';
 
 export const esmToCjs = iife(
   ({ transformSync } = require('@babel/core')) =>
@@ -24,42 +25,86 @@ export interface BuildArgs {
 }
 
 export async function build(args: BuildArgs): Promise<Error | boolean> {
-  const [srcFiles] = await Promise.all([
-    readdirDeep(join(import.meta.dir, '../src'), { ignore: /\.test\.ts$/ }).then(({ files }) => files),
-  ]);
-
-  if (args.target === 'bun') return new Error('Bun target is no longer supported, use browser instead.');
-
+  const srcFiles = iter(globIterateSync(join(import.meta.dir, '../src/**/*.ts')))
+    .filter(path => !path.endsWith('test.ts'))
+    .toArray();
   const tsconfig = join(import.meta.dir, `../tsconfig.${args.format}.json`);
+  const distPath = join(import.meta.dir, '../dist');
 
-  const buildResult = await esbuild({
-    // entryPoints: [join(import.meta.dir, '../src/index.ts')],
-    entryPoints: srcFiles,
-    outdir: `./dist/${args.outdir}`,
-    sourceRoot: './src',
-    sourcemap: 'linked',
-    // minify: true,
-    minifyWhitespace: false,
-    minifySyntax: true,
-    minifyIdentifiers: false,
-    platform: args.target,
-    format: args.format,
-    treeShaking: true,
-    splitting: args.format === 'esm',
-    bundle: true,
-    // external: ['argparse'],
-    define: {
-      'Bun.env.RUNTIME': `'${args.target}'`,
-    },
-    tsconfig,
-    plugins: [dtsPlugin({ tsconfig })],
-  });
+  if (args.target === 'bun') {
+    const outdir = join(distPath, args.outdir);
+    const buildResult = await Bun.build({
+      ...args,
+      entrypoints: srcFiles,
+      outdir,
+      sourcemap: 'external',
+      // @ts-expect-error Just let bun handle the error if it's not a valid target.
+      format: args.format,
+      target: args.target,
+      minify: true,
+      root: './src',
+      splitting: true,
+      define: {
+        'Bun.env.RUNTIME': `'${args.target}'`,
+      },
+    });
+    if (!buildResult.success) {
+      console.error(buildResult.logs.map(v => v.message).join('\n'));
+      return new Error(`Build for ${args.outdir} failed.`);
+    }
+    if (buildResult.logs.length) {
+      let level: 'warn' | 'log' | 'error' | 'debug' = 'log';
+      const logs = iter(buildResult.logs)
+        .tap(log => {
+          if (log.level === 'error') level = 'error';
+          else if (log.level === 'warning' && level !== 'error') level = 'warn';
+          else if (log.level === 'debug' && level !== 'warn') level = 'debug';
+        })
+        .map(v => v.message)
+        .join('\n');
+      console[level](logs);
+    }
 
-  if (buildResult.errors.length) {
-    console.error(buildResult.errors);
-    return new Error('Build failed.');
+    // Copy declaration files:
+    console.log(join(distPath, 'types'));
+    await iter(globIterateSync(join(distPath, 'types') + '/*'))
+      .tap(console.log)
+      .map(src => {
+        const relativePath = relative(join(distPath, 'types'), src);
+        console.log(relativePath);
+        return copyFile(src, join(outdir, relativePath));
+      })
+      .promiseAll();
+  } else {
+    const buildResult = await esbuild({
+      // entryPoints: [join(import.meta.dir, '../src/index.ts')],
+      entryPoints: srcFiles,
+      outdir: `./dist/${args.outdir}`,
+      sourceRoot: './src',
+      sourcemap: 'linked',
+      // minify: true,
+      minifyWhitespace: false,
+      minifySyntax: true,
+      minifyIdentifiers: false,
+      platform: args.target,
+      format: args.format,
+      treeShaking: true,
+      splitting: args.format === 'esm',
+      bundle: true,
+      // external: ['argparse'],
+      define: {
+        'Bun.env.RUNTIME': `'${args.target}'`,
+      },
+      tsconfig,
+      plugins: [dtsPlugin({ tsconfig })],
+    });
+
+    if (buildResult.errors.length) {
+      console.error(buildResult.errors);
+      return new Error(`Build for ${args.outdir} failed.`);
+    }
+    if (buildResult.warnings.length) console.warn(buildResult.warnings);
   }
-  if (buildResult.warnings.length) console.warn(buildResult.warnings);
 
   // Build types only:
   // ok(await sh('bunx tsc -p tsconfig.types-only.json'));
@@ -153,13 +198,13 @@ export async function build(args: BuildArgs): Promise<Error | boolean> {
 
 if (import.meta.main) {
   await $`rm -rf dist`;
+  await $`tsc -p tsconfig.types.json`;
 
   await iter([
     { target: 'node', format: 'cjs', outdir: 'node-cjs' },
     { target: 'browser', format: 'esm', outdir: 'browser' },
-    // { target: 'node', format: 'esm', outdir: 'node-esm' },
+    { target: 'bun', format: 'esm', outdir: 'bun' },
   ] satisfies BuildArgs[])
     .map(args => build(args).then(ok))
-    .concat([iife(() => $`tsc -p tsconfig.types.json`)])
     .promiseAll();
 }
