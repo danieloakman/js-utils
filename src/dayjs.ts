bc3984a41f105e91d7c101ccf52e951df9dcf32e
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-10-01 05:29:08 +1000

    Include dayjs side-effect imports

--- src/dayjs.ts
+++ src/dayjs.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-duplicate-imports */
+// Duplicate side-effect imports are necessary to import the 'dayjs' module interface overloads and include them in the build.
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+import 'dayjs/plugin/utc';
 import duration from 'dayjs/plugin/duration';
+import 'dayjs/plugin/duration';
 import isBetween from 'dayjs/plugin/isBetween';
+import 'dayjs/plugin/isBetween';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/plugin/relativeTime';
 import minMax from 'dayjs/plugin/minMax';
+import 'dayjs/plugin/minMax';
 
 // Apply plugins:
 dayjs.extend(utc);
@@ -12,6 +19,8 @@ dayjs.extend(isBetween);
 dayjs.extend(relativeTime);
 dayjs.extend(minMax);
 
+type ExtendedDayjs = ReturnType<typeof dayjs>;
+
 export const Dayjs = dayjs;
-export type Dayjs = ReturnType<typeof dayjs>;
+export type Dayjs = ExtendedDayjs;
 export default Dayjs;
