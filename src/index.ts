8c9aea2a4218d944f7cd79067b5ffef258a57484
Daniel Brown <42539848+danieloakman@users.noreply.github.com>
2025-01-13 11:45:34 +1100

    Add caching to index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './args';
 export * from './assertions';
 export * from './BinarySearch';
+export * from './caching';
 export * from './dayjs';
 export * from './functional';
 export * from './InMemoryCache';
