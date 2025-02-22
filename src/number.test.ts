813530fffc2ffa63893ffd54d4613a299a632a74
Daniel Brown <42539848+danieloakman@users.noreply.github.com>
2025-02-23 09:48:08 +1100

    Add rountToNearest

--- src/number.test.ts
+++ src/number.test.ts
@@ -1,4 +1,4 @@
-import { manhattanDistance, roundTo } from '.';
+import { manhattanDistance, roundTo, roundToNearest } from '.';
 import { describe, expect, it } from 'bun:test';
 
 describe('number', () => {
@@ -16,5 +16,17 @@ describe('number', () => {
     expect(manhattanDistance([1, 2, 3], [1, 2, 4])).toBe(1);
     expect(manhattanDistance([1, 2, 3], [1, 2, 2])).toBe(1);
     expect(manhattanDistance([1, 2, 3], [1, 1, 3])).toBe(1);
+  });
+
+  it('roundToNearest', () => {
+    expect(roundToNearest(1.2345, 0.1)).toBe(1.2);
+    expect(roundToNearest(1.2345, 0.01)).toBe(1.23);
+    expect(roundToNearest(1.2345, 0.001)).toBe(1.235);
+    expect(roundToNearest(1.2345, 0.0001)).toBe(1.2345);
+    expect(roundToNearest(1.2345, 0.00001)).toBe(1.2345);
+    expect(roundToNearest(50, 30)).toBe(60);
+    expect(roundToNearest(100, 30)).toBe(90);
+    expect(roundToNearest(100, 99)).toBe(99);
+    expect(roundToNearest(100, 2)).toBe(100);
   });
 });
