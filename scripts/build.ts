d077f9020f443ce15732c790881a25ec578a0583
Daniel Brown <42539848+danieloakman@users.noreply.github.com>
2025-03-08 06:38:07 +1100

    Include dayjs as an external dependency

--- scripts/build.ts
+++ scripts/build.ts
@@ -8,7 +8,6 @@ import { globIterateSync } from 'glob';
 import { iter } from 'iteragain';
 import { join, relative } from 'path';
 import { transformSync } from '@babel/core';
-
 export const esmToCjs = (code: string): string => {
   return ok(
     // @ts-expect-error
@@ -93,7 +92,7 @@ export async function build(args: BuildArgs): Promise<Result<boolean>> {
       treeShaking: true,
       splitting: args.format === 'esm',
       bundle: true,
-      // external: ['argparse'],
+      external: ['dayjs'],
       define: {
         'Bun.env.RUNTIME': `'${args.target}'`,
       },
@@ -107,93 +106,6 @@ export async function build(args: BuildArgs): Promise<Result<boolean>> {
     }
     if (buildResult.warnings.length) console.warn(buildResult.warnings);
   }
-
-  // Build types only:
-  // ok(await sh('bunx tsc -p tsconfig.types-only.json'));
-
-  // Build types and index.js with typescript first to create the declaration files:
-  // ok(await sh(`bunx tsc -p tsconfig.${args.format}.json --emitDeclarationOnly`));
-
-  // Include the declaration files in the build and make sure that the index.js file is in there:
-  // ok(await sh(`bunx tsc -p tsconfig.${args.format}.json`));
-  // for await (const { path, stats } of walkdir(join(import.meta.dir, '../'), { ignore: /node_modules/i })) {
-  //   if (!stats.isFile() || !path.endsWith('.js')) continue;
-  //   // Remove all js files except index.js:
-  //   if (!path.endsWith('index.js')) await rm(path);
-  // }
-
-  // const buildResult = await Bun.build({
-  //   minify: false,
-  //   // If bundling into one index.js:
-  //   // entrypoints: [join(import.meta.dir, '../src/index.ts')],
-  //   entrypoints: srcFiles,
-  //   outdir: '.',
-  //   splitting: true,
-  //   external: ['arg-parse'],
-  //   // TODO: once bun supports cjs as a format, then put args.format here. For now babel is being used to transpile to cjs.
-  //   format: 'esm',
-  //   target: args.target,
-  //   root: './src',
-  //   // sourcemap: 'external',
-  //   define: {
-  //     'Bun.env[\'RUNTIME\']': `'${args.target}'`,
-  //     'parseArgs': '() => { throw new Error("Can\'t parse args in browser.") }',
-  //   },
-  // });
-
-  // if (buildResult.logs.length) console.log(buildResult.logs);
-
-  // if (!existsSync(join(import.meta.dir, '../index.js')))
-  //   return new Error('Build did not contain `index.js` file, aborting.');
-
-  // if (!buildResult.success) return new Error('Build failed.');
-
-  // // Add empty js files for all ts files that were not compiled:
-  // const srcdir = join(import.meta.dir, '../src');
-  // for (const srcFile of srcFiles) {
-  //   const expectedJsFile = relative(srcdir, srcFile.replace(/\.ts$/, '.js'));
-  //   if (!existsSync(expectedJsFile)) {
-  //     await Bun.write(expectedJsFile, '');
-  //     // return new Error(`Build did not contain ${expectedJsFile}`);
-  //   }
-  // }
-
-  // console.log(
-  //   'Compiled:',
-  //   await readdirDeep(process.cwd(), { ignore: /node_modules/ }).then(({ files }) =>
-  //     files.filter(v => v.endsWith('.js')).map(v => relative(process.cwd(), v)),
-  //   ),
-  // );
-
-  // /** Up to date file text contents. Because buildResult.outputs *could* contain an out of date blob. */
-  // const buildOutputs = buildResult.outputs.map(v => ({ path: v.path, text: () => Bun.file(v.path).text() }));
-
-  // // Post build processing and checks:
-  // for (const output of buildOutputs) {
-  //   const text = await output.text();
-  //   const m = toArray(filterMap(matches(/export {[^}]+}/g, text), toMatch));
-  //   // Remove all exports except the last one:
-  //   if (m.length >= 2) {
-  //     console.log('Found multiple exports in', output.path);
-  //     const l = last(m) ?? raise('Could not find last');
-  //     await Bun.write(output.path, stringSplice(text, l.start, l.length + 1));
-  //   }
-  // }
-
-  // if (args.format === 'cjs') {
-  //   // Transpile to cjs:
-  //   for (const output of buildOutputs) {
-  //     const transpiled = pipe(
-  //       await output.text(),
-  //       esmToCjs,
-  //       str => str.replaceAll('import.meta.require', 'require'),
-  //       // minify,
-  //       // ok,
-  //       // v => v.code,
-  //     );
-  //     await Bun.write(output.path, transpiled);
-  //   }
-  // }
 
   return Result.Ok(true);
 }
