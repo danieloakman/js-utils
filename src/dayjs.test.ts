d254cae8ce564912f62b13ea5f813e6fea86f6c2
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-05-26 09:51:06 +1000

    add a test

--- src/dayjs.test.ts
+++ src/dayjs.test.ts
@@ -11,5 +11,6 @@ describe('dayjs', () => {
     expect(yesterday.isBetween(year2000, dayjs())).toBeTruthy();
     expect(dayjs.min(yesterday, year2000)).toEqual(year2000);
     expect(dayjs.max(yesterday, year2000)).toEqual(yesterday);
+    expect(dayjs('2026-05-26T08:00:00-07:00').tz('America/Los_Angeles').format()).toEqual('2026-05-26T08:00:00-07:00');
   });
 });
